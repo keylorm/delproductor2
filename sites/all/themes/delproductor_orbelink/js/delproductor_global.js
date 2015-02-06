ff67b8d74f8da7f23cca2ddc78387bf886707e43
Keylor Mora <keylor@orbelink.com>
2015-02-06 14:32:52 -0600

    first commit

---
+++ sites/all/themes/delproductor_orbelink/js/delproductor_global.js
@@ -0,0 +1,30 @@
+jQuery.noConflict(); 
+jQuery(document).ready(function(){
+	
+	jQuery('#zone-menu-wrapper #block-system-main-menu .menu .mp').hover(
+         function () {
+		 	var subm = jQuery(this).find(".menu");
+			if(subm[0] != undefined){
+				if(jQuery(this).hasClass('menu-expandido-doble')){
+					jQuery('body').addClass('mopen2');
+				}				
+				jQuery('body').addClass('mopen');
+				subm.show('slow');
+			}
+			//console.log(subm[0]);
+         }, 
+         function () {
+		 	var subm = jQuery(this).find(".menu");
+			if(subm[0] != undefined){
+				jQuery('body').removeClass('mopen');
+				jQuery('body').removeClass('mopen2');
+				subm.hide('slow');
+			}			
+			//console.log(subm[0]);
+         }
+     );
+	
+	
+	
+	
+});
