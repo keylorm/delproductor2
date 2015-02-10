67cd4398f7bbad49a96ef0e6e8fae1dba4a1b8d8
Keylor Mora <keylor@orbelink.com>
2015-02-10 17:26:35 -0600

    breadcrumb

--- sites/all/themes/delproductor_orbelink/template.php
+++ sites/all/themes/delproductor_orbelink/template.php
@@ -57,3 +57,17 @@ function delproductor_orbelink_form_alter(&$form, &$form_state, $form_id){
     $form['submit']['#value'] = "Buscar";
   }
 }
+
+
+function delproductor_orbelink_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    $output .= '<div class="breadcrumb">' . implode(' ', $breadcrumb) . '</div>';
+    return $output;
+  }
+}
