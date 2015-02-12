cd5351ac740c5c28018fac77a3ef8e404eca8207
Keylor Mora <keylor@orbelink.com>
2015-02-12 14:47:20 -0600

    inicio de productos

--- sites/all/themes/delproductor_orbelink/template.php
+++ sites/all/themes/delproductor_orbelink/template.php
@@ -45,6 +45,7 @@ function delproductor_orbelink_preprocess_node(&$variables) {
 function delproductor_orbelink_preprocess_page(&$variables) {
     $variables['page']['content']['preface']['#grid_container'] = 0;
     $variables['page']['content']['content']['#grid_container'] = 0;
+    $variables['page']['content']['content_below']['#grid_container'] = 0;
     $variables['page']['content']['postscript']['#grid_container'] = 0;
   
 }
